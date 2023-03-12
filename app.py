0fed3d5d4136abc9726eaa3f22d1d4f98bf06d1a
vankha09052007 <127587432+vankha09052007@users.noreply.github.com>
2023-03-12 21:29:07 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+import sklearn
+import plotly
+st.title("Giải phương trình bậc nhất")
+a=st.number_input("Tham số a")
+b = st.number_input("Tham số b")
+if st.button("Giải"):
+  if a == 0:
+    if b == 0:
+      st.write("Phương trình có vô số nghiệm")
+    else:
+      st.write("Phương trình vô nghiệm")
+  else:
+    st.write("Phương trình có 1 nghiệm", -b/a) 
